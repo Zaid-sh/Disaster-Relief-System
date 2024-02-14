954497b18a1eabb19b3912b0223da64d8aad83f2
Mohammed Zaid Shaikh <mohammed.shaikh1@ucalgary.ca>
2024-02-14 11:47:10 -0700

    Some changes to the DisasterVictim class

--- 32/edu/ucalgary/oop/DisasterVictim.java
+++ 32/edu/ucalgary/oop/DisasterVictim.java
@@ -3,5 +3,17 @@ package edu.ucalgary.oop;
 import java.util.regex.*;
 
 public class DisasterVictim{
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String comments;
+    private int assignedSocialID;
+    private MedicalRecord[] MedicalRecord;
+    private FamilyRelations[] familyConnection;
+    private String entryDate;
+    private Supply[] personalBelongings;
+    private String gender;
+    private int counter;
+
 
 }
