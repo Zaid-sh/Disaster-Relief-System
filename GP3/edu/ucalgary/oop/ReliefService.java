55c272f80455805cd7a0849ad54a85821d267df6
Neha Parmar <neha04parmar@gmail.com>
2024-02-16 12:42:34 -0700

    fixed relief service

--- GP3/edu/ucalgary/oop/ReliefService.java
+++ GP3/edu/ucalgary/oop/ReliefService.java
@@ -1,20 +1,16 @@
 package edu.ucalgary.oop;
 
-import java.util.regex.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class ReliefService {
-    // Member Variables
     private Inquirer inquirer;
     private DisasterVictim missingPerson;
     private String dateOfInquiry;
     private String infoProvided;
     private Location lastKnownLocation;
 
-    // Constructor
-    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson,String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
-        if (!dateOfInquiry.matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")) {
-            throw new IllegalArgumentException("Date of birth entered incorrectly.");
-        }
+    public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided, Location lastKnownLocation) {
         this.inquirer = inquirer;
         this.missingPerson = missingPerson;
         this.dateOfInquiry = dateOfInquiry;
@@ -22,26 +18,52 @@ public class ReliefService {
         this.lastKnownLocation = lastKnownLocation;
     }
 
-    // Setters
-    public void setInquirer(Inquirer inquirer) {this.inquirer = inquirer; }
-    public void setMissingPerson(DisasterVictim missingPerson) {this.missingPerson = missingPerson; }
-    public void setDateOfInquiry(String dateOfInquiry) throws IllegalArgumentException{
-        if (!dateOfInquiry.matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")) {
-            throw new IllegalArgumentException("Date of birth entered incorrectly.");
-            }
-        this.dateOfInquiry = dateOfInquiry;
-        }
-    public void setInfoProvided(String infoProvided) {this.infoProvided = infoProvided; }
-    public void setLastKnownLocation(Location lastKnownLocation) {this.lastKnownLocation = lastKnownLocation; }
+    public Inquirer getInquirer() {
+        return inquirer;
+    }
 
-    // Getters
-    public Inquirer getInquirer() {return this.inquirer; }
-    public DisasterVictim getMissingPerson() {return this.missingPerson; }
-    public String getDDateOfInquiry() {return this.dateOfInquiry; }
-    public String getInfoProvided() {return this.infoProvided; }
-    public Location getLastKnownLocation() {return this.lastKnownLocation; }
+    public void setInquirer(Inquirer inquirer) {
+        this.inquirer = inquirer;
+    }
 
-    public String getLogDetails() {return null; }
-    //what do we return for this setter?
-    //potentially log details we can get from a value from a diff class?
+    public DisasterVictim getMissingPerson() {
+        return missingPerson;
+    }
+
+    public void setMissingPerson(DisasterVictim missingPerson) {
+        this.missingPerson = missingPerson;
+    }
+
+    public String getDateOfInquiry() {
+        return dateOfInquiry;
+    }
+
+    public void setDateOfInquiry(String dateOfInquiry) {
+    try {
+        LocalDate.parse(dateOfInquiry);
+    } catch (DateTimeParseException e) {
+        throw new IllegalArgumentException("Invalid date format for date of inquiry");
+    }
+    this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getInfoProvided() {
+        return infoProvided;
+    }
+
+    public void setInfoProvided(String infoProvided) {
+        this.infoProvided = infoProvided;
+    }
+
+    public Location getLastKnownLocation() {
+        return lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(Location lastKnownLocation) {
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public String getLogDetails() {
+        return "Inquirer: " + inquirer.getFirstName() + ", Missing Person: " + missingPerson.getFirstName() + ", Date of Inquiry: " + dateOfInquiry + ", Info Provided: " + infoProvided + ", Last Known Location: " + lastKnownLocation.getName();
+    }
 }
